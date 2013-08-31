b482f7a6df5b1ed93bcf31ff57579b43aabcb107
Michal Bachman <git@bachman.cz>
2013-08-31 10:38:46 +0100

    perf tests from master

---
+++ src/test/java/com/graphaware/relcount/perf/NoPropsWritePerformanceTest.java
@@ -0,0 +1,74 @@
+package com.graphaware.relcount.perf;
+
+import com.graphaware.framework.GraphAwareFramework;
+import com.graphaware.relcount.full.module.FullRelationshipCountModule;
+import com.graphaware.relcount.simple.module.SimpleRelationshipCountModule;
+import com.graphaware.test.TestUtils;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.neo4j.graphdb.GraphDatabaseService;
+
+import java.io.IOException;
+
+@Ignore
+public class NoPropsWritePerformanceTest extends RelationshipWritePerformanceTest {
+
+    @Test
+    public void plainDatabase() throws IOException {
+        System.out.println("Plain Database:");
+        measure(new DatabaseModifier() {
+            @Override
+            public void alterDatabase(GraphDatabaseService database) {
+                //do nothing
+            }
+        }, "hundredNodesNoPropsPlainDatabaseWrite");
+    }
+
+    @Test
+    public void emptyFramework() throws IOException {
+        System.out.println("Empty Framework:");
+        measure(new DatabaseModifier() {
+            @Override
+            public void alterDatabase(GraphDatabaseService database) {
+                GraphAwareFramework framework = new GraphAwareFramework(database);
+                framework.start();
+            }
+        }, "hundredNodesNoPropsEmptyFrameworkWrite");
+    }
+
+    @Test
+    public void simpleRelcount() throws IOException {
+        System.out.println("Simple Relcount:");
+        measure(new DatabaseModifier() {
+            @Override
+            public void alterDatabase(GraphDatabaseService database) {
+                GraphAwareFramework framework = new GraphAwareFramework(database);
+                framework.registerModule(new SimpleRelationshipCountModule());
+                framework.start();
+            }
+        }, "hundredNodesNoPropsSimpleRelcountWrite");
+    }
+
+    @Test
+    public void fullRelcount() throws IOException {
+        System.out.println("Full Relcount:");
+        measure(new DatabaseModifier() {
+            @Override
+            public void alterDatabase(GraphDatabaseService database) {
+                GraphAwareFramework framework = new GraphAwareFramework(database);
+                framework.registerModule(new FullRelationshipCountModule());
+                framework.start();
+            }
+        }, "hundredNodesNoPropsFullRelcountWrite");
+    }
+
+    @Override
+    protected long doMeasureCreatingRelationships(final GraphDatabaseService database, final int number, final int batchSize) {
+        return TestUtils.time(new TestUtils.Timed() {
+            @Override
+            public void time() {
+                createRelationships(number, batchSize, database);
+            }
+        });
+    }
+}
---
+++ src/test/java/com/graphaware/relcount/perf/PerformanceTest.java
@@ -0,0 +1,87 @@
+package com.graphaware.relcount.perf;
+
+import com.graphaware.tx.executor.NullItem;
+import com.graphaware.tx.executor.batch.NoInputBatchTransactionExecutor;
+import com.graphaware.tx.executor.batch.UnitOfWork;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Random;
+
+import static org.neo4j.graphdb.DynamicRelationshipType.withName;
+
+public abstract class PerformanceTest {
+
+    protected static final String CONFIG = "src/test/resources/neo4j-perf.properties";
+
+    protected static final Random RANDOM = new Random(51823591465L);
+
+    protected static final int HUNDRED = 100;
+    protected static final int THOUSAND = 1000;
+    protected static final int HUNDRED_THOUSAND = HUNDRED * 1000;
+
+    protected void resultsToFile(Map<String, String> results, String fileName) {
+        try {
+            File file = new File("src/test/resources/perf/" + fileName + "-" + System.currentTimeMillis() + ".txt");
+            file.createNewFile();
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            for (Map.Entry<String, String> entry : results.entrySet()) {
+                bw.write(entry.getKey() + entry.getValue());
+                bw.newLine();
+            }
+
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected interface DatabaseModifier {
+        void alterDatabase(GraphDatabaseService database);
+    }
+
+    protected void twoProps(Relationship rel) {
+        rel.setProperty("rating", RANDOM.nextInt(4) + 1);
+        rel.setProperty("timestamp", RANDOM.nextLong());
+    }
+
+    protected void twoMoreProps(Relationship rel) {
+        rel.setProperty("3", RANDOM.nextLong());
+        rel.setProperty("4", RANDOM.nextLong());
+    }
+
+    protected void createNodes(GraphDatabaseService databaseService, int number) {
+        new NoInputBatchTransactionExecutor(databaseService, THOUSAND, number, new UnitOfWork<NullItem>() {
+            @Override
+            public void execute(GraphDatabaseService database, NullItem input, int batchNumber, int stepNumber) {
+                database.createNode();
+            }
+        }).execute();
+    }
+
+    protected void createRelationships(int noRels, final int batchSize, GraphDatabaseService database) {
+        new NoInputBatchTransactionExecutor(database, batchSize, noRels, new UnitOfWork<NullItem>() {
+            @Override
+            public void execute(GraphDatabaseService database, NullItem input, int batchNumber, int stepNumber) {
+                final Node node1 = database.getNodeById(RANDOM.nextInt(HUNDRED) + 1);
+                final Node node2 = database.getNodeById(RANDOM.nextInt(HUNDRED) + 1);
+
+                Relationship rel = node1.createRelationshipTo(node2, withName("TEST" + ((batchSize * (batchNumber - 1) + stepNumber) % 2)));
+                createRelPropsIfNeeded(rel);
+            }
+        }).execute();
+    }
+
+    protected void createRelPropsIfNeeded(Relationship rel) {
+        //none by default
+    }
+}
