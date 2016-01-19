416d34e5c175affebdc27863e09321fa46e36114
Michal Bachman <michal@graphaware.com>
2016-01-19 15:38:55 +0800

    assert json with JSONAssert

--- src/test/java/com/graphaware/module/relcount/RelcountIntegrationTest.java
+++ src/test/java/com/graphaware/module/relcount/RelcountIntegrationTest.java
@@ -18,11 +18,12 @@ package com.graphaware.module.relcount;
 
 import com.graphaware.test.integration.NeoServerIntegrationTest;
 import org.eclipse.jetty.http.HttpStatus;
+import org.json.JSONException;
 import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 import java.io.IOException;
 
-import static com.graphaware.test.util.TestUtils.assertJsonEquals;
 import static com.graphaware.test.util.TestUtils.jsonAsString;
 
 /**
@@ -31,13 +32,13 @@ import static com.graphaware.test.util.TestUtils.jsonAsString;
 public class RelcountIntegrationTest extends NeoServerIntegrationTest {
 
     @Test
-    public void relationshipCountsShouldBeCachedWhenRuntimeAndRelcountAreEnabled() throws InterruptedException, IOException {
+    public void relationshipCountsShouldBeCachedWhenRuntimeAndRelcountAreEnabled() throws InterruptedException, IOException, JSONException {
         httpClient.post(baseUrl() + "/db/data/transaction/commit", jsonAsString("create"), HttpStatus.OK_200);
 
-        assertJsonEquals(httpClient.post(baseUrl() + "/db/data/transaction/commit", jsonAsString("query"), HttpStatus.OK_200),
-                "{\"results\":[{\"columns\":[\"one._GA_relcount_\"],\"data\":[{\"row\":[[17,1,1,43,2,3,1,33,4,18,5,0,12,6,7,82,-79,2,2]]}]}],\"errors\":[]}");
+        JSONAssert.assertEquals(httpClient.post(baseUrl() + "/db/data/transaction/commit", jsonAsString("query"), HttpStatus.OK_200),
+                "{\"results\":[{\"columns\":[\"one._GA_relcount_\"],\"data\":[{\"row\":[[17,1,1,43,2,3,1,33,4,18,5,0,12,6,7,82,-79,2,2]]}]}],\"errors\":[]}", false);
 
-        assertJsonEquals(httpClient.post(baseUrl() + "/db/data/transaction/commit", jsonAsString("query2"), HttpStatus.OK_200),
-                "{\"results\":[{\"columns\":[\"two._GA_relcount_\"],\"data\":[{\"row\":[[17,1,1,43,2,3,2,33,4,18,5,0,12,6,7,82,-79,2,2]]}]}],\"errors\":[]}");
+        JSONAssert.assertEquals(httpClient.post(baseUrl() + "/db/data/transaction/commit", jsonAsString("query2"), HttpStatus.OK_200),
+                "{\"results\":[{\"columns\":[\"two._GA_relcount_\"],\"data\":[{\"row\":[[17,1,1,43,2,3,2,33,4,18,5,0,12,6,7,82,-79,2,2]]}]}],\"errors\":[]}", false);
     }
 }
